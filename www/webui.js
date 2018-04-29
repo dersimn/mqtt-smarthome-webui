e9d05b6bcbacfdebd8dfbf720f9648e688bb9d46
Simon Christmann <simon@christmann.email>
2018-04-29 14:38:09 +0200

    Indentation to spaces

--- www/webui.js
+++ www/webui.js
@@ -1,16 +1,16 @@
 // Page Switching
 $(window).on('hashchange',function(){ 
-	gotToPage(location.hash || '#mainpage');
+    gotToPage(location.hash || '#mainpage');
 });
 function gotToPage(pageHash) {
-	console.log('gotToPage', pageHash);
-	$(".sh-page").each(function() {
-		if ($(this).is(pageHash)) {
-			$(this).css('left','0%');
-		} else {
-			$(this).css('left','100%');
-		}
-	});
+    console.log('gotToPage', pageHash);
+    $(".sh-page").each(function() {
+        if ($(this).is(pageHash)) {
+            $(this).css('left','0%');
+        } else {
+            $(this).css('left','100%');
+        }
+    });
 }
 
 // Data loading
@@ -19,33 +19,33 @@ $.getJSON('data.json', function(data) {
 
     // Preflight data
     $.each(data.pages, function(i, page) {
-    	if (page.pageid == 'mainpage') { data.pages[i].mainpage = true; }
+        if (page.pageid == 'mainpage') { data.pages[i].mainpage = true; }
 
-    	$.each(data.pages[i].items, function(j, item) {
-    		data.pages[i].items[j]['itemtype_'+item.type] = true;
+        $.each(data.pages[i].items, function(j, item) {
+            data.pages[i].items[j]['itemtype_'+item.type] = true;
 
-    		if (item.type == 'switch') { data.pages[i].items[j].switchId = shortId(); };
-    	});
+            if (item.type == 'switch') { data.pages[i].items[j].switchId = shortId(); };
+        });
     });
 
-	$(function() {
-		// Mustache
-		var template = $('#pageTemplate').html();
-		var rendered = Mustache.render(template, data);
-    	$('body').append(rendered);
+    $(function() {
+        // Mustache
+        var template = $('#pageTemplate').html();
+        var rendered = Mustache.render(template, data);
+        $('body').append(rendered);
 
-    	gotToPage(window.location.hash || '#mainpage');
+        gotToPage(window.location.hash || '#mainpage');
 
 
-    	// MQTT
-    	client = new Paho.MQTT.Client(location.hostname, Number(location.port), '/mqtt');
-    	client.onConnectionLost = function() {
-    		// Handle online/offline Button
-    		$('[data-mqtt-state]').removeClass('btn-outline-success').addClass('btn-outline-secondary').text('Offline');
-    	};
-    	client.connect({onSuccess:function() {
-    		// Handle online/offline Button
-    		$('[data-mqtt-state]').removeClass('btn-outline-secondary').addClass('btn-outline-success').text('Online');
-    	}});
+        // MQTT
+        client = new Paho.MQTT.Client(location.hostname, Number(location.port), '/mqtt');
+        client.onConnectionLost = function() {
+            // Handle online/offline Button
+            $('[data-mqtt-state]').removeClass('btn-outline-success').addClass('btn-outline-secondary').text('Offline');
+        };
+        client.connect({onSuccess:function() {
+            // Handle online/offline Button
+            $('[data-mqtt-state]').removeClass('btn-outline-secondary').addClass('btn-outline-success').text('Online');
+        }});
     });
 });
